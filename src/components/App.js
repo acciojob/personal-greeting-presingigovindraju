dbdab1b7140b8218e3180be7c2dfc0e7455c0a65
presingi_govind_raju <presingigovindraju333@gmail.com>
2024-01-15 05:04:59 +0530

    greetings

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
 
-import React from "react";
+import React,{useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [msg,setMsg] = useState("")
   return (
     <div>
-        {/* Do not remove the main div */}
+      <p>Enter your name:</p>
+        <input onChange={(e)=>setMsg(e.target.value)}/>
+        <p>{msg}</p>
     </div>
   )
 }
