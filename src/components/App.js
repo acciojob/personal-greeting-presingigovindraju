d74c863eb840799d3bca3a74c0ab52b0d2602476
presingi_govind_raju <presingigovindraju333@gmail.com>
2024-01-16 17:46:57 +0530

    s

--- src/components/App.js
+++ src/components/App.js
@@ -4,14 +4,14 @@ import './../styles/App.css';
 
 const App = () => {
   const [msg,setMsg] = useState("")
-  function him(e){
-    setMsg(e.target.value)
-  }
+  // function him(e){
+  //   setMsg(e.target.value)
+  // }
   return (
     <div>
         <p>Enter your name:</p>
         <form>
-             <input type="text"  onChange={him}/>
+             <input type="text"  onChange={(e)=>setMsg(e.target.value)}/>
         </form>
         {
           msg!=""?<p>Hello {msg}!</p>:""
